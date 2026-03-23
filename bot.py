9f1df9d26023b968d08f1e481f1dce58747b6df2
KIASAMA <469200612@qq.com>
2026-03-23 17:36:28 +0800

    0323

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-
+from nonebot.adapters.console import Adapter as Console
 
 
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
+driver.register_adapter(Console)
 
 
 nonebot.load_from_toml("pyproject.toml")
