6ff7f17039e962dd3d6d7b9bfba2d94de095931c
KIASAMA <469200612@qq.com>
2026-03-24 17:39:21 +0800

    0324

--- src/plugins/ping_test/__init__.py
+++ src/plugins/ping_test/__init__.py
@@ -1,5 +1,7 @@
 from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
+from nonebot.plugin.on import on_message
+from nonebot.rule import to_me
 
 from .config import Config
 
@@ -12,3 +14,9 @@ __plugin_meta__ = PluginMetadata(
 
 config = get_plugin_config(Config)
 
+
+message_machter = on_message(rule=to_me(), priority=999, block=True)
+
+@message_machter.handle()
+async def handle_message():
+        await message_machter.finish("窝嫩叠")
